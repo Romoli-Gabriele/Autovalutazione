84dda1ddf84ba17db3d0e6f5871ea8ae3498e302
Gabriele Romoli <gh05romoli@gmail.com>
2020-11-10 11:58:27 +0100

    Contatore funzionante

---
+++ Contatore.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contatore;
+
+/**
+ *
+ * @author gabbo
+ */
+public class Contatore {
+public int c;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 0;
+        for(int i=0;i<10;i++){
+        a += 1;
+        System.out.println(a);
+        }
+    }
+    
+}
